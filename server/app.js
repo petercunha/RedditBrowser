10eaddd16904d1f944fb7da3e15d943ca3faa419
Peter Cunha <petercunha8@gmail.com>
2018-07-22 12:06:53 -0700

    (Server) Add compression, error handling, and send permalink to posts in JSON.

--- server/app.js
+++ server/app.js
@@ -1,19 +1,17 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
-var cors = require('cors')
+const createError = require('http-errors')
+const compression = require('compression')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const cors = require('cors')
 
-var indexRouter = require('./routes/index')
+const indexRouter = require('./routes/index')
 
 var app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
-
-app.use(logger('dev'))
+app.use(compression())
+app.use(logger('short'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
@@ -23,12 +21,12 @@ app.use(cors())
 app.use('/', indexRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 	next(createError(404))
 })
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
 	// set locals, only providing error in development
 	res.locals.message = err.message
 	res.locals.error = req.app.get('env') === 'development' ? err : {}
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,33 +4,39 @@ const axios = require('axios')
 var router = express.Router()
 
 /* GET home page */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 	res.json('ok')
 })
 
 /* GET subreddit */
-router.get('/:subreddit', function(req, res, next) {
-	axios
-		.get(`https://www.reddit.com/r/${req.params.subreddit}.json`)
-		.then(r => {
-			const json = r.data
-			const posts = json.data.children
-			let responseBuffer = []
+router.get('/:subreddit', function (req, res, next) {
+	try {
+		axios
+			.get(`https://www.reddit.com/r/${req.params.subreddit}.json`)
+			.then(
+				r => {
+					const json = r.data
+					const posts = json.data.children
+					let responseBuffer = []
 
-			posts.map(post => {
-				const chunk = {
-					title: post.data.title,
-					url: post.data.url
+					posts.map(post => {
+						const chunk = {
+							url: post.data.url,
+							title: post.data.title,
+							link: post.data.permalink
+						}
+						responseBuffer.push(chunk)
+					})
+
+					res.json(responseBuffer)
+				},
+				err => {
+					res.json('An error occured while accessing the Reddit API.')
 				}
-				responseBuffer.push(chunk)
-			})
-
-			res.send(responseBuffer)
-		})
-		.catch(e => {
-			res.send(e)
-			console.log('error:', e)
-		})
+			)
+	} catch (unknownErr) {
+		res.json('An unknown error occured.')
+	}
 })
 
 module.exports = router
