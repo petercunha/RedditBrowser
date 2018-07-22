97a7ac5d191b85596ebe1452f64e4cc67b04fde7
Peter Cunha <petercunha8@gmail.com>
2018-07-22 11:36:08 -0700

    Improve deploy script, add it to package.json

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,20 @@
-rm -rf ./build && yarn build && surge ./build redditbrowser.surge.sh
+echo
+echo '\t---------------------------------------'
+echo '\t\tDeleting old build folder'
+echo '\t----------------------------------------'
+echo
+echo $ rm -rf ./build
+echo
+rm -rf ./build
+
+echo '\t----------------------------------------'
+echo '\t\tRebuilding React project'
+echo '\t----------------------------------------'
+echo
+yarn build
+
+echo
+echo '\t----------------------------------------'
+echo '\t\tDeploying static assets'
+echo '\t----------------------------------------'
+surge ./build redditbrowser.surge.sh
